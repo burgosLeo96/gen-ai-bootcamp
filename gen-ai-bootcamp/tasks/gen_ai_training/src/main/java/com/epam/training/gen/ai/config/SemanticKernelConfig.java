17c3dfefcd8c80de24d126f62d25a9d977d77ab2
Leonard Burgos <burgosleonard96.lb@gmail.com>
2024-11-18 12:10:06 -0500

    Removed KernelPlugin.

--- gen-ai-bootcamp/tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/config/SemanticKernelConfig.java
+++ gen-ai-bootcamp/tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/config/SemanticKernelConfig.java
@@ -1,12 +1,9 @@
 package com.epam.training.gen.ai.config;
 
-import com.epam.training.gen.ai.config.plugin.LightsPlugin;
 import com.microsoft.semantickernel.Kernel;
 import com.microsoft.semantickernel.orchestration.InvocationContext;
 import com.microsoft.semantickernel.orchestration.InvocationReturnMode;
 import com.microsoft.semantickernel.orchestration.ToolCallBehavior;
-import com.microsoft.semantickernel.plugin.KernelPlugin;
-import com.microsoft.semantickernel.plugin.KernelPluginFactory;
 import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +13,9 @@ public class SemanticKernelConfig {
 
     @Bean
     public Kernel semanticKernel(ChatCompletionService chatCompletionService) {
-        KernelPlugin lightPlugin = KernelPluginFactory.createFromObject(new LightsPlugin(),
-                "LightsPlugin");
 
         return Kernel.builder()
                 .withAIService(ChatCompletionService.class, chatCompletionService)
-                .withPlugin(lightPlugin)
                 .build();
     }
 

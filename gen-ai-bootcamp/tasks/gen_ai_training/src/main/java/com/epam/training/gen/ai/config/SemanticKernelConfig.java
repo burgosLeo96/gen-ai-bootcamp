3cdd9c2433a05cf35575616eed50f5443871f225
burgosLeo96 <34455274+burgosLeo96@users.noreply.github.com>
2024-12-16 15:57:16 -0500

    Complimented code to complete task # 2. Added Prompt execution settings. (#2)

--- gen-ai-bootcamp/tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/config/SemanticKernelConfig.java
+++ gen-ai-bootcamp/tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/config/SemanticKernelConfig.java
@@ -3,7 +3,9 @@ package com.epam.training.gen.ai.config;
 import com.microsoft.semantickernel.Kernel;
 import com.microsoft.semantickernel.orchestration.InvocationContext;
 import com.microsoft.semantickernel.orchestration.InvocationReturnMode;
+import com.microsoft.semantickernel.orchestration.PromptExecutionSettings;
 import com.microsoft.semantickernel.orchestration.ToolCallBehavior;
+import com.microsoft.semantickernel.orchestration.responseformat.ResponseFormat;
 import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +15,6 @@ public class SemanticKernelConfig {
 
     @Bean
     public Kernel semanticKernel(ChatCompletionService chatCompletionService) {
-
         return Kernel.builder()
                 .withAIService(ChatCompletionService.class, chatCompletionService)
                 .build();
@@ -23,7 +24,16 @@ public class SemanticKernelConfig {
     public InvocationContext invocationContext() {
         return new InvocationContext.Builder()
                 .withReturnMode(InvocationReturnMode.LAST_MESSAGE_ONLY)
+                .withPromptExecutionSettings(initializeExecutionSettings())
                 .withToolCallBehavior(ToolCallBehavior.allowAllKernelFunctions(true))
+                .build();
+    }
+
+    private PromptExecutionSettings initializeExecutionSettings() {
+        return PromptExecutionSettings
+                .builder()
+                .withTemperature(0.7)
+                .withResponseFormat(ResponseFormat.Type.JSON_OBJECT)
                 .build();
     }
 }
